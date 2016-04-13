da05bcada4f0edaee5c60584e23c6a9e495ee2c3
zane <neuschuler@gmail.com>
2016-04-12 23:09:35 -0700

    Lab 5
    I think from now on my instructor is just going to have us do this stuff
    in one tabbed frame.

---
+++ Lab 5/src/lab5/Lab5.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5;
+
+/**
+ *
+ * @author zane
+ */
+public class Lab5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Lab5Form MemeForm = new Lab5Form();
+        MemeForm.setVisible(true);
+    }
+    
+}
